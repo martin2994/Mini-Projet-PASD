69687261bc73c4f02c7f9d8089624462e758001b
Martin <martin2994@hotmail.fr>
2015-11-03 17:13:59 +0100

    1er commit interpreter

--- PASD_mini-projet_sujet/interpreter.c
+++ PASD_mini-projet_sujet/interpreter.c
@@ -7,20 +7,52 @@
 
 # include "interpreter.h"
 
-
-
 # undef NDEBUG   // FORCE ASSERT ACTIVATION
 
 
 
-
+//quoi faire pour la trace
 void interprete_chunk ( chunk ch ,
-			interpretation_context ic )  {}
+			interpretation_context ic )  
+{
+	if(chunk_is_value(ch) )
+	{
+		linked_list_chunk_add_front ( ic->stack , ch ); //j'empile les value
+	}else if( chunk_is_operator ( ch ) )
+	{
+		
+		basic_type bt=operator_evaluate (ch,ic);  //j'evalue l'operation
+		chunk ch=(chunk)malloc(sizeof(struct chunk_struct));
+		*(basic_type*)ch->state=bt;
+		linked_list_chunk_add_front ( ic->stack , ch );//j'empile le res 
+	}
+}
 
 void interprete_chunk_list ( linked_list_chunk ch ,
-			     interpretation_context ic )  {}
-
+			     interpretation_context ic )  
+{
+	linked_list_chunk llctmp=linked_list_chunk_create ();
+	while(!linked_list_chunk_is_empty (ch) )//transfert de ch à llctmp en interpretant le chunk
+	{
+		chunk chtmp=linked_list_chunk_pop_front (ch);
+		interprete_chunk(chtmp,ic);
+		linked_list_chunk_add_back(llctmp,chtmp);
+	}while(!linked_list_chunk_is_empty (llctmp) ) //retransfert de llctmp à ch
+	{
+		linked_list_chunk_add_back(ch,linked_list_chunk_pop_front (llctmp)); 
+	}
+}
+/*tant que le fichier est pas vide on lit et on en fait des chunk(ou une liste) qu'on interprete*/
 void interprete ( FILE * f ,
-		  bool do_trace )  {}
+		  bool do_trace )  
+{
+	interpretation_context ic=(interpretation_context)malloc(sizeof(struct interpretation_context_struct));
+	ic->stack=linked_list_chunk_create();
+	ic->dic=dictionary_create();
+	ic->do_trace=do_trace;
+	do{
+		interprete_chunk(read_chunk_io(f),ic);
+	}while(read_chunk_io (f) != END OF FILE);
+}
 
 
